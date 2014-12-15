1b57673971862d30e900222c2c8248880b9acf5d
cavedude00@gmail.com <cavedude00@gmail.com@ca82a947-cafe-b94e-75fc-165f3d6f6a81>
2014-12-15 17:09:45 +0000

    Changed some branding.

--- templates/intro.tmpl.php
+++ templates/intro.tmpl.php
@@ -1,5 +1,5 @@
   <center>
-    <div class="page_title"><span><br>Project Everquest Online Editing Interface</span></div>
+    <div class="page_title"><span><br>Everquest Mac Online Editing Interface</span></div>
     <img src="images/dontpanic.jpg" alt="Don't Panic">
-    <div class="page_title"><span>PEQ Official SVN Revision <a href="http://code.google.com/p/peqphpeditor" target="_blank"><?=$current_revision?></a></span></div>
+    <div class="page_title"><span>EQMac Official SVN Revision <a href="http://code.google.com/p/takpphpeditor/" target="_blank"><?=$current_revision?></a></span></div>
   </center>
