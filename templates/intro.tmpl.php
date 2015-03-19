a5acae62806d0f0ba2973aa66b7fd1c9cf98f15d
regneq <rregenn@gmail.com>
2015-03-18 21:40:14 -0700

    change intro to EQMac and add link

--- templates/intro.tmpl.php
+++ templates/intro.tmpl.php
@@ -1,5 +1,5 @@
   <center>
     <div class="page_title"><span><br>Everquest Mac Online Editing Interface</span></div>
     <img src="images/dontpanic.jpg" alt="Don't Panic">
-    <div class="page_title"><span>PEQ Official Revision<br/><a href="https://github.com/ProjectEQ/peqphpeditor" target="_blank"><?=$current_revision?></a></span></div>
+    <div class="page_title"><span>EQMac Official Revision<br/><a href="https://github.com/EQMacEmu/takpphpeditor" target="_blank"><?=$current_revision?></a></span></div>
   </center>
