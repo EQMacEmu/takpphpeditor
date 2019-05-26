5233b7bf0ce7f696c036b7a7f021091cf8e8155a
regneq <rregenn@gmail.com>
2019-05-26 13:50:25 -0700

    Fix some defaults to get rid of erroneous behavior with invalid data. Prefer mysqli functionality. Old mysql functionality will be deprecated soon.

--- ajax.php
+++ ajax.php
@@ -1,18 +1,18 @@
 <?php
 
-$editor     = (isset($_GET['editor']) ? $_GET['editor'] : '');
-$action     = (isset($_GET['action']) ? $_GET['action'] : 0);
-$npcid      = (isset($_GET['npcid']) ? $_GET['npcid'] : null);
-$z          = (isset($_GET['z']) ? $_GET['z'] : '');
-$zoneid     = (isset($_GET['zoneid']) ? $_GET['zoneid'] : '');
-$fid        = (isset($_GET['fid']) ? $_GET['fid'] : '');
-$tskid      = (isset($_GET['tskid']) ? $_GET['tskid'] : '');
-$ts         = (isset($_GET['ts']) ? $_GET['ts'] : '');
-$rec        = (isset($_GET['rec']) ? intval($_GET['rec']) : '0');
-$spellset   = (isset($_GET['spellset']) ? $_GET['spellset'] : '');
-$playerid   = (isset($_GET['playerid']) ? $_GET['playerid'] : null);
-$acctid     = (isset($_GET['acctid']) ? $_GET['acctid'] : null);
-$guildid    = (isset($_GET['guildid']) ? $_GET['guildid'] : null);
-$aaid       = (isset($_GET['aaid']) ? $_GET['aaid'] : null);
+$editor = (isset($_GET['editor'])) ? $_GET['editor'] : null;
+$action = (isset($_GET['action'])) ? intval($_GET['action']) : 0;
+$npcid = (isset($_GET['npcid'])) ? intval($_GET['npcid']) : 0;
+$z = (isset($_GET['z'])) ? $_GET['z'] : null;
+$zoneid = (isset($_GET['zoneid'])) ? intval($_GET['zoneid']) : 0;
+$fid = (isset($_GET['fid'])) ? intval($_GET['fid']) : 0;
+$tskid = (isset($_GET['tskid'])) ? intval($_GET['tskid']) : 0;
+$ts = (isset($_GET['ts'])) ? intval($_GET['ts']) : 0;
+$rec = (isset($_GET['rec'])) ? intval($_GET['rec']) : 0;
+$spellset = (isset($_GET['spellset'])) ? intval($_GET['spellset']) : 0;
+$playerid = (isset($_GET['playerid'])) ? intval($_GET['playerid']) : 0;
+$acctid = (isset($_GET['acctid'])) ? intval($_GET['acctid']) : 0;
+$guildid = (isset($_GET['guildid'])) ? intval($_GET['guildid']) : 0;
+$aaid = (isset($_GET['aaid'])) ? intval($_GET['aaid']) : 0;
 
 ?>
