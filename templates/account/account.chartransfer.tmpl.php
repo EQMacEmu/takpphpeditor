72d699408aeaea488beded25e0e635184099bddb
Nazwadi <nazwadi@pm.me>
2023-12-25 20:44:25 -0500

    Cleans up syntax to conform to HTML5 and PHP8.0 standards

--- templates/account/account.chartransfer.tmpl.php
+++ templates/account/account.chartransfer.tmpl.php
@@ -1,11 +1,12 @@
-<?
+<?php
+  $acctid = $acctid ?? 0;
   $from_acct = getAccountName($acctid);
 ?>
   <div id="searchblock" style="display:none;">
-    <center>
+    <div class="center">
       <iframe src="templates/iframes/accountsearch.php" style="display:block;"></iframe>
       <input type="button" value="Hide Search" onclick="hideSearch();">
-    </center><br/>
+    </div><br/>
   </div>
   <div class="table_container" style="width:250px;">
     <div class="table_header">
@@ -13,16 +14,16 @@
     </div>
     <div class="table_content">
       <form id="transfer" method="post" action="index.php?editor=account&acctid=<?=$acctid?>&playerid=<?=$_GET['playerid']?>&action=6">
-        <center>
-          Transfer <strong><font color="green"><?=getPlayerName($_GET['playerid'])?></font></strong> from<br/><br/>
+        <div class="center">
+          Transfer <span style="font-weight:bold;color: green"><?=getPlayerName($_GET['playerid'])?></span> from<br/><br/>
           <strong><?=$from_acct?></strong><br/><br/>
-          to<br/><br/>
-          <input type="text" size="20" value="Select account" readonly="true" id="to_text" name="tacct" onClick="showSearch();">
+            <label for="to_text">to</label><br/><br/>
+          <input type="text" size="20" value="Select account" readonly="readonly" id="to_text" name="tacct" onClick="showSearch();">
           <input type="hidden" id="from_acct" value="<?=$from_acct?>">
           <div id="submitblock" style="display:none;">
             <br/><br/><input type="button" value="GO" onClick="validateTransfer();">&nbsp;<input type="button" onClick="history.go(-2);" value="Cancel">
           </div>
-        </center>
+        </div>
       </form>
     </div>
   </div>
