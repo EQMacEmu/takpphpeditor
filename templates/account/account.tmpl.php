1cbdccd7de513da76dbb0c4f7ac8f7e9f78be65e
Nazwadi <nazwadi@pm.me>
2023-12-25 22:36:49 -0500

    Cleans up syntax (HTML5/PHP8.0) for account.tmpl.php

--- templates/account/account.tmpl.php
+++ templates/account/account.tmpl.php
@@ -1,101 +1,119 @@
-  <div class="table_container">
+<div class="table_container">
     <div class="table_header">
-      <div style="float:right">
-        <a onClick="javascript:alert('Not yet!');"><img src="images/c_table.gif" border="0" title="Edit this Account" /></a>
-<?if ($character_array) {
-    echo '<a onClick="javascript:alert(\'Unable to delete account yet. Delete all characters associated with this account first!\');"><img src="images/table.gif" border="0" title="Characters Still Exist on this Account!" /></a>';
-  }
-  else {
-    echo '<a onClick="return confirm(\'Really delete account ' . trim($name) . ' (' . $acctid . ')?\');" href="index.php?editor=account&acctid=' . $acctid . '&action=4"><img src="images/table.gif" border="0" title="Delete this Account" /></a>';
-  }
-?>
-      </div>
-      <?=$id?> - <?echo trim($name);?>
+        <div style="float:right">
+            <a onClick="alert('Not yet!');"><img src="images/c_table.gif" alt="Table Icon" style="border:0;"
+                                                 title="Edit this Account"/></a>
+            <?php global $character_array;
+            if (!empty($character_array)) { // This behavior doesn't seem to work correctly yet, and it is not clear how $character_array is set.
+                echo '<a onClick="alert(\'Unable to delete account yet. Delete all characters associated with this account first!\');"><img src="images/table.gif" alt="Edit Table" style="border:0;" title="Characters Still Exist on this Account!" /></a>';
+            } else {
+                $name = $name ?? "";
+                echo '<a onClick="return confirm(\'Really delete account ' . trim($name) . ' (' . ($acctid ?? "") . ')?\');" href="index.php?editor=account&acctid=' . ($acctid ?? "") . '&action=4"><img src="images/table.gif" alt="Crossed Out Table Icon" style="border:0;" title="Delete this Account" /></a>';
+            }
+            ?>
+        </div>
+        <?= $id ?? "" ?> - <?php echo trim(($name ?? "")); ?>
     </div>
     <div class="table_content">
-<?if ($online) echo "<h2><center><font color='red'>WARNING! THIS ACCOUNT IS ONLINE...</font></center></h2>";?>
-      <table cellspacing="0" border="0" width="100%">
-        <tr>
-          <td width="100%">
-            <table cellspacing="0" border="0" width="100%">
-              <tr>
-                <td width="45%">
-                  <fieldset>
-                    <legend><strong>Account Info</strong></legend>
-                    Account ID: <?=$id?><br/>
-                    LS Name: <?=$name?><br/>
-                    LS ID: <?=$lsaccount_id?><br/>
-                    Password: <?=$password?><br/>
-                    <a href="index.php?editor=account&acctid=<?=$id?>&action=7" title="Edit Account Status">Status</a>: <?=$status?><br/>
-                    GM Speed: <?=$yesno[$gmspeed]?><br/>
-                    Hide Me: <?=$yesno[$hideme]?><br/>
-                    Revoked: <?=$yesno[$revoked]?><br/>
-                    Karma: <?=$karma?><br/>
-                    Rules Flag: <?=$rulesflag?><br/>
-                    Shared Platinum: <?=$sharedplat?><br/>
-                    Minilogin IP: <?=$minilogin_ip?><br/>
-                    Suspended: <?echo ($suspendeduntil > 0) ? $suspendeduntil : "N/A";?><br/>
-                    Suspend Reason <?echo ($suspend_reason != "") ? $suspend_reason : "N/A";?><br/>
-                    Ban Reason: <?echo ($ban_reason != "") ? $ban_reason : "N/A";?><br/>
-                    Account Created: <?=get_real_time($time_creation)?>
-                  </fieldset>
-                </td>
-                <td width="55%" rowspan="2">
-                  <fieldset>
-                    <legend><strong>IP Address Info</strong></legend>
-                    <table>
-                      <tr>
-                        <td width="40%"><center>IP Address</center></td>
-                        <td width="20%"><center>Login Count</center></td>
-                        <td width="40%"><center>Last Login</center></td>
-                      </tr>
-<?
-  if ($ips) {
-    foreach ($ips as $ip_address) {
-      echo '<tr>';
-      echo '<td width="40%"><center>' . $ip_address['ip'] . '</center></td>';
-      echo '<td width="20%"><center>' . $ip_address['count'] . '</center></td>';
-      echo '<td width="40%"><center>' . $ip_address['lastused'] . '</center></td>';
-      echo '</tr>';
-    }
-  }
-?>
+        <?php if (isset($online) && $online) echo "<h2><div class='center'><span class='alert-danger'>WARNING! THIS ACCOUNT IS ONLINE...</span></div></h2>"; ?>
+        <table style="border-collapse: collapse; border-spacing: 0; border: 0; width: 100%;">
+            <tr>
+                <td style="width: 100%;">
+                    <table style="border-collapse: collapse; border-spacing: 0; border: 0; width: 100%;">
+                        <tr>
+                            <td style="width:45%">
+                                <fieldset>
+                                    <legend><strong>Account Info</strong></legend>
+                                    Account ID: <?= $id ?? "N/A" ?><br/>
+                                    LS Name: <?= $name ?? "N/A" ?><br/>
+                                    LS ID: <?= $lsaccount_id ?? "N/A" ?><br/>
+                                    Password: <?= $password ?? "N/A" ?><br/>
+                                    <a href="index.php?editor=account&acctid=<?= $id ?? "" ?>&action=7"
+                                       title="Edit Account Status">Status</a>: <?= $status ?? "N/A" ?><br/>
+                                    <?php if (isset($yesno)): ?>
+                                        GM Speed: <?= (isset($gmspeed) ? $yesno[$gmspeed] : "unk") ?><br/>
+                                        Hide Me: <?= (isset($hideme) ? $yesno[$hideme] : "unk") ?><br/>
+                                        Revoked: <?= (isset($revoked) ? $yesno[$revoked] : "unk") ?><br/>
+                                    <?php endif ?>
+                                    Karma: <?= $karma ?? "" ?><br/>
+                                    Rules Flag: <?= $rulesflag ?? "" ?><br/>
+                                    Shared Platinum: <?= $sharedplat ?? "" ?><br/>
+                                    Minilogin IP: <?= $minilogin_ip ?? "" ?><br/>
+                                    Suspended: <?php echo (isset($suspendeduntil) && $suspendeduntil > 0) ? $suspendeduntil : "N/A"; ?>
+                                    <br/>
+                                    Suspend
+                                    Reason <?php echo (isset($suspend_reason) && $suspend_reason != "") ? $suspend_reason : "N/A"; ?>
+                                    <br/>
+                                    Ban
+                                    Reason: <?php echo (isset($ban_reason) && $ban_reason != "") ? $ban_reason : "N/A"; ?>
+                                    <br/>
+                                    Account
+                                    Created: <?= (isset($time_creation) ? get_real_time($time_creation) : "Unknown") ?>
+                                </fieldset>
+                            </td>
+                            <td style="width:55%;" rowspan="2">
+                                <fieldset>
+                                    <legend><strong>IP Address Info</strong></legend>
+                                    <table>
+                                        <tr>
+                                            <td style="width: 40%;">
+                                                <div class="center">IP Address</div>
+                                            </td>
+                                            <td style="width: 20%;">
+                                                <div class="center">Login Count</div>
+                                            </td>
+                                            <td style="width: 40%;">
+                                                <div class="center">Last Login</div>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                        if (isset($ips) && $ips) {
+                                            foreach ($ips as $ip_address) {
+                                                echo '<tr>';
+                                                echo '<td style="width: 40%;"><div class="center">' . $ip_address['ip'] . '</div></td>';
+                                                echo '<td style="width: 20%;"><div class="center">' . $ip_address['count'] . '</div></td>';
+                                                echo '<td style="width: 40%;"><div class="center">' . $ip_address['lastused'] . '</div></td>';
+                                                echo '</tr>';
+                                            }
+                                        }
+                                        ?>
+                                    </table>
+                                </fieldset>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <fieldset>
+                                    <legend><strong>Characters</strong></legend>
+                                    <?php
+                                    if (isset($characters) && $characters) {
+                                        $count = 0;
+                                        echo '<table style="border-collapse: collapse; border-spacing: 0; border: 0; width: 100%;">';
+                                        foreach ($characters as $character) {
+                                            $count++;
+                                            echo '<tr>';
+                                            echo '<td style="width: 25%;">Character' . $count . ': </td>';
+                                            echo '<td style="width: 60%;">';
+                                            echo ($character['id'] != '') ? '<a href="index.php?editor=player&playerid=' . $character['id'] . '">' . $character['name'] . '</a>  (' . $character['id'] . ')' : 'EMPTY';
+                                            echo '</td>';
+                                            echo '<td style="width: 15%; text-align: right;"><a href="index.php?editor=account&acctid=' . ($acctid ?? "") . '&playerid=' . $character['id'] . '&action=5"><img src="images/user.gif" alt="User" height="13" width="13" title="Transfer this Character"></a> <a onClick="return confirm(\'Really delete player ' . $character['name'] . ' (' . $character['id'] . ') from this account?\');" href="index.php?editor=player&playerid=' . $character['id'] . '&acctid=' . ($id ?? "") . '&action=4"><img src="images/remove.gif" alt="Remove" title="Delete this Character"></a></td>';
+                                            echo '</tr>';
+                                        }
+                                        echo '</table>';
+                                    } else {
+                                        echo '<br/><br/><div class="center">NO CHARACTERS ON THIS ACCOUNT</div><br/>';
+                                    }
+                                    echo '<br/>';
+                                    ?>
+                                    Last Character
+                                    Used: <?php echo (isset($charname) && $charname != '') ? $charname : 'Never Logged a Character'; ?>
+                                    <br/>
+                                </fieldset>
+                            </td>
+                        </tr>
                     </table>
-                  </fieldset>
                 </td>
-              </tr>
-              <tr>
-                <td>
-                  <fieldset>
-                    <legend><strong>Characters</strong></legend>
-<?
-  if ($characters) {
-    $count = 0;
-    echo '<table cellspacing="0" border="0" width="100%">';
-    foreach ($characters as $character) {
-      $count++;
-      echo '<tr>';
-      echo '<td width="25%">Character' . $count . ': </td>';
-      echo '<td width="60%">';
-      echo ($character['id'] != '') ? '<a href="index.php?editor=player&playerid=' . $character['id'] . '">' . $character['name'] . '</a>  (' . $character['id'] . ')' : 'EMPTY';
-      echo '</td>';
-      echo '<td width="15%" align="right"><a href="index.php?editor=account&acctid=' . $acctid . '&playerid=' . $character['id'] . '&action=5"><img src="images/user.gif" height="13" width="13" title="Transfer this Character"></a> <a onClick="return confirm(\'Really delete player ' . $character['name'] . ' (' . $character['id'] . ') from this account?\');" href="index.php?editor=player&playerid=' . $character['id'] . '&acctid=' . $id . '&action=4"><img src="images/remove.gif" title="Delete this Character"></a></td>';
-      echo '</tr>';
-    }
-    echo '</table>';
-  }
-  else {
-    echo '<br/><br/><center>NO CHARACTERS ON THIS ACCOUNT</center><br/>';
-  }
-  echo '<br/>';
-?>
-                    Last Character Used: <?echo ($charname != '') ? $charname : 'Never Logged a Character';?><br/>
-                  </fieldset>
-                </td>
-              </tr>
-            </table>
-          </td>
-        </tr>
-      </table>
+            </tr>
+        </table>
     </div>
-  </div>
+</div>
