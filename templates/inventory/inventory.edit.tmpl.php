37e9abff6dd1f2c1efe516b6d2ee1d1b475eb008
Nazwadi <nazwadi@pm.me>
2024-01-01 20:11:57 -0500

    Updates inventory templates to PHP 8.0 / HTML5 syntax

--- templates/inventory/inventory.edit.tmpl.php
+++ templates/inventory/inventory.edit.tmpl.php
@@ -1,59 +1,73 @@
-      <table class="edit_form" width="300px">
-        <tr>
-          <td class="edit_form_header">
-            Inventory item located at <?=getPlayerName($inv_item['id'])?>'s <?=$slots[$inv_item['slotid']]?>
-          </td>
-        </tr>
-        <tr>
-          <td class="edit_form_content">
-            <form name="inventory" id="inventory" method="POST" action="index.php?editor=inv&playerid=<?=$inv_item['id']?>&slotid=<?=$inv_item['slotid']?>&action=7">
-              <table width="100%" cellpadding="3" cellspacing="0">
-                <tr>
-                  <td>
-                    Player ID:<br>
-                    <input type="text" size="5" name="charid" value="<?=$inv_item['id']?>" readonly="true">
-                  </td>
-                  <td>
-                    <a title="22-29: Main Inventory Slots">Slot ID:</a><br/>
-                    <input type="text" size="3" name="slotid" value="<?=$inv_item['slotid']?>" readonly="true">
-                  </td>
-                  <td>
-                    Item ID:<br>
-                    <input type="text" size="5" name="itemid" value="<?=$inv_item['itemid']?>">
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    Charges:<br>
-                    <input type="text" size="3" name="charges" value="<?=$inv_item['charges']?>">
-                  </td>
-                  <td colspan="2">
-                    Color:<br>
-                    <input type="text" size="10" name="color" value="<?=$inv_item['color']?>">
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    Inst NoDrop:<br>
-                    <input type="checkbox" name="instnodrop"<?echo ($inv_item['instnodrop'] == 1) ? " checked" : "";?>>
-                  </td>
-                </tr>
-                <tr>
-                  <td colspan="3">
-                    Custom Data:<br>
-                    <input type="text" size="37" name="custom_data" value="<?=$inv_item['custom_data']?>">
-                  </td>
-                </tr>
-                <tr><td>&nbsp;</td></tr>
-                <tr>
-                  <td colspan="3">
-                    <center>
-                      <input type="submit" name="submit" value="Submit Changes">&nbsp;<input type="button" name="cancel" value="Cancel" onClick="history.back();">
-                    </center>
-                  </td>
-                </tr>
-              </table>
+<table class="edit_form" style="width: 300px;">
+    <tr>
+        <td class="edit_form_header">
+            Inventory item located at <?= getPlayerName($inv_item['id']) ?? "Unknown Player" ?>
+            's <?= $slots[$inv_item['slotid']] ?? "Unknown Slot" ?>
+        </td>
+    </tr>
+    <tr>
+        <td class="edit_form_content">
+            <form name="inventory" id="inventory" method="POST"
+                  action="index.php?editor=inv&playerid=<?= $inv_item['id'] ?? "" ?>&slotid=<?= $inv_item['slotid'] ?? "" ?>&action=7">
+                <table style="width: 100%; border-collapse: collapse; border-spacing: 0;">
+                    <tr>
+                        <td style="padding: 3px;">
+                            <label for="charid">Player ID:</label><br>
+                            <input type="text" size="5" id="charid" name="charid" value="<?= $inv_item['id'] ?? "" ?>"
+                                   readonly="readonly">
+                        </td>
+                        <td style="padding: 3px;">
+                            <a title="22-29: Main Inventory Slots"><label for="slotid">Slot ID:</label></a><br/>
+                            <input type="text" size="3" id="slotid" name="slotid"
+                                   value="<?= $inv_item['slotid'] ?? "" ?>" readonly="readonly">
+                        </td>
+                        <td style="padding: 3px;">
+                            <label for="itemid">Item ID:</label><br>
+                            <input type="text" size="5" id="itemid" name="itemid"
+                                   value="<?= $inv_item['itemid'] ?? "" ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 3px;">
+                            <label for="charges">Charges:</label><br>
+                            <input type="text" size="3" id="charges" name="charges"
+                                   value="<?= $inv_item['charges'] ?? "" ?>">
+                        </td>
+                        <td colspan="2" style="padding: 3px;">
+                            <label for="color">Color:</label><br>
+                            <input type="text" size="10" id="color" name="color"
+                                   value="<?= $inv_item['color'] ?? "" ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 3px;">
+                            <label for="instnodrop">Inst NoDrop:</label><br>
+                            <input type="checkbox" id="instnodrop"
+                                   name="instnodrop"<?php echo (!empty($inv_item) && $inv_item['instnodrop'] == 1) ? " checked" : ""; ?>>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3" style="padding: 3px;">
+                            <label for="custom_data">Custom Data:</label><br>
+                            <input type="text" size="37" id="custom_data" name="custom_data"
+                                   value="<?= $inv_item['custom_data'] ?? "" ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 3px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3" style="padding: 3px;">
+                            <div class="center">
+                                <input type="submit" name="submit" value="Submit Changes">&nbsp;<input type="button"
+                                                                                                       name="cancel"
+                                                                                                       value="Cancel"
+                                                                                                       onClick="history.back();">
+                            </div>
+                        </td>
+                    </tr>
+                </table>
             </form>
-          </td>
-        </tr>
-      </table>
+        </td>
+    </tr>
+</table>
--- templates/inventory/inventory.player.tmpl.php
+++ templates/inventory/inventory.player.tmpl.php
@@ -1,42 +1,53 @@
-    <div class="table_container" style="width: 500px;">
-      <div class="table_header">
-        <table width="100%" cellpadding="0" cellspacing="0">
-          <tr>
-            <td align="left" width="33%">Player Inventory - <a href="index.php?editor=player&playerid=<?=$playerid?>"><?=getPlayerName($playerid)?></a></td>
-            <td align="right" width="5%"><a href="index.php?editor=inv&playerid=<?=$playerid?>&action=4"><img src="images/add.gif" border="0" title="Create a new entry" /></a>
-          </tr>
+<div class="table_container" style="width: 500px;">
+    <div class="table_header">
+        <table style="width: 100%; border-collapse: collapse; border-spacing: 0;">
+            <tr>
+                <td style="padding:0 ; text-align: left; width: 33%;">Player Inventory - <a
+                            href="index.php?editor=player&playerid=<?= $playerid ?? "" ?>"><?= getPlayerName($playerid ?? '') ?? "Unknown" ?></a>
+                </td>
+                <td style="padding: 0; text-align: right; width: 5%;">
+                    <a href="index.php?editor=inv&playerid=<?= $playerid ?? "" ?>&action=4"><img src="images/add.gif" style="border: 0;" alt="Yellow Plus Icon" title="Create a new entry"/></a>
+            </tr>
         </table>
-      </div>
-      <table class="table_content2" width="100%">
-<?
-$x = 0;
-if ($inventory):
-?>
-        <tr>
-          <td align="center"><strong>Slot</strong></td>
-          <td align="center"><strong>Item</strong></td>
-          <td width="10%">&nbsp;</td>
-        </tr>
-<?
-endif;
-if ($inventory):
-  foreach ($inventory as $inv):
-    extract($inv);
-?>
-        <tr bgcolor="#<? echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA";?>">
-          <td>&nbsp;&nbsp;&nbsp;<?=$inv['slotid']?> - <?=$slots[$inv['slotid']]?></td>
-          <td align="center"><?=get_item_name($inv['itemid'])?> (<?=$inv['itemid']?>) - [<a href="http://lucy.allakhazam.com/item.html?id=<?=$inv['itemid']?>" target="_blank">Lucy</a>]</td>
-          <td align="right"><a href="index.php?editor=inv&playerid=<?=$playerid?>&slotid=<?=$inv['slotid']?>&action=6"><img src="images/edit2.gif" width="13" height="13" border="0" title="View/Edit Entry"></a>&nbsp;<a onClick="return confirm('Really delete this entry?');" href="index.php?editor=inv&playerid=<?=$playerid?>&slotid=<?=$inv['slotid']?>&action=8"><img src="images/remove3.gif" border="0" title="Delete Entry"></a></td>
-        </tr>
-<?
-    $x++;
-  endforeach;
-endif;
-if ($x == 0):
-?>
-        <tr>
-          <td colspan="3" align="left" width="100" style="padding: 10px;">No Inventory</td>
-        </tr>
-<?endif;?>
-      </table>
     </div>
+    <table class="table_content2" style="width: 100%;">
+        <?php
+        $x = 0;
+        if (!empty($inventory)):
+            ?>
+            <tr>
+                <td style="text-align: center;"><strong>Slot</strong></td>
+                <td style="text-align: center;"><strong>Item</strong></td>
+                <td style="width: 10%;">&nbsp;</td>
+            </tr>
+        <?php
+        endif;
+        if (!empty($inventory)):
+            foreach ($inventory as $inv):
+                extract($inv);
+                ?>
+                <tr style="background-color: #<?php echo ($x % 2 == 0) ? "BBBBBB" : "AAAAAA"; ?>">
+                    <td>&nbsp;&nbsp;&nbsp;<?= $inv['slotid'] ?> - <?= $slots[$inv['slotid']] ?></td>
+                    <td style="text-align: center;"><?= get_item_name($inv['itemid']) ?> (<?= $inv['itemid'] ?>) - [<a
+                                href="https://lucy.allakhazam.com/item.html?id=<?= $inv['itemid'] ?>" target="_blank">Lucy</a>]
+                    </td>
+                    <td style="text-align: right;"><a
+                                href="index.php?editor=inv&playerid=<?= $playerid ?>&slotid=<?= $inv['slotid'] ?>&action=6"><img
+                                    src="images/edit2.gif" width="13" height="13" style="border: 0;" alt="Edit Entry Icon"
+                                    title="View/Edit Entry"></a>&nbsp;<a
+                                onClick="return confirm('Really delete this entry?');"
+                                href="index.php?editor=inv&playerid=<?= $playerid ?>&slotid=<?= $inv['slotid'] ?>&action=8"><img
+                                    src="images/remove3.gif" style="border: 0;" alt="Red X Icon" title="Delete Entry"></a></td>
+                </tr>
+                <?php
+                $x++;
+            endforeach;
+        endif;
+        if ($x == 0):
+            ?>
+            <tr>
+                <td colspan="3" style="text-align: left; width: 100%; padding: 10px;">No Inventory</td>
+            </tr>
+        <?php endif; ?>
+    </table>
+</div>
