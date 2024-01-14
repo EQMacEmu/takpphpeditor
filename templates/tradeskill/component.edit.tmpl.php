bd4c1082953a10a20ee270282cb5772b5fd83479
Nazwadi <nazwadi@pm.me>
2024-01-14 17:43:27 -0500

    Updates syntax for HTML5/PHP 8.0 in tradeskills; removes unused learned recipes

--- templates/tradeskill/component.edit.tmpl.php
+++ templates/tradeskill/component.edit.tmpl.php
@@ -1,38 +1,38 @@
-      <center>
+      <div class="center">
         <iframe id='searchframe' src='templates/iframes/itemsearch.php'></iframe>
         <input id="button" type="button" value='Hide Item Search' onclick='hideSearch();' style='display:none; margin-bottom: 20px;'>
-      </center>
+      </div>
       <form name="component" method="post" action="index.php?editor=tradeskill&ts=<?=$ts?>&rec=<?=$rec?>&id=<?=$id?>&action=6">
         <div class="edit_form" style="width: 200px;">
           <div class="edit_form_header">Edit Recipe Component</div>
           <div class="edit_form_content">
-            <strong>Item ID:</strong> (<a href="javascript:showSearch();">search</a>)<br>
-            <input class="indented" id="id" type="text" name="item_id" size="7" value="<?=$item_id?>"><br><br>
+              <strong><label for="id">Item ID:</label></strong> (<a href="javascript:showSearch();">search</a>)<br>
+            <input class="indented" id="id" type="text" name="item_id" size="7" value="<?=$item_id ?? ""?>"><br><br>
             <fieldset>
-              <legend><strong><font size="-1">Containers:</font></strong></legend>
-              Is this the container?<br>
-              <select class="indented" name='iscontainer'>
-                <option value="0"<?echo($iscontainer == 0) ? " selected" : ""?>>no</option>
-                <option value="1"<?echo($iscontainer == 1) ? " selected" : ""?>>yes</option>
+              <legend><strong><span style="font-size: 13px;">Containers:</span></strong></legend>
+                <label for="iscontainer">Is this the container?</label><br>
+              <select class="indented" id="iscontainer" name='iscontainer'>
+                <option value="0"<?php echo(isset($iscontainer) && $iscontainer == 0) ? " selected" : ""?>>no</option>
+                <option value="1"<?php echo(isset($iscontainer) && $iscontainer == 1) ? " selected" : ""?>>yes</option>
               </select>
             </fieldset><br><br>
             <fieldset>
-              <legend><strong><font size="-1">Components:</font></strong></legend>
-              Qty Required:<br>
-              <input class="indented" type="text" name="componentcount" size="7" value="<?=$componentcount?>"><br><br>
-              Qty Returned on Fail:<br>
-              <input class="indented" type="text" name="failcount" size="7" value="<?=$failcount?>"><br><br>
+              <legend><strong><span style="font-size: 13px;">Components:</span></strong></legend>
+                <label for="componentcount">Qty Required:</label><br>
+              <input class="indented" type="text" id="componentcount" name="componentcount" size="7" value="<?=$componentcount ?? ""?>"><br><br>
+                <label for="failcount">Qty Returned on Fail:</label><br>
+              <input class="indented" type="text" id="failcount" name="failcount" size="7" value="<?=$failcount ?? ""?>"><br><br>
             </fieldset><br><br>
             <fieldset>
-              <legend><strong><font size="-1">Products:</font></strong></legend>
-              Qty Produced: <br>
-              <input class="indented" type="text" name="successcount" size="7" value="<?=$successcount?>">
+              <legend><strong><span style="font-size: 13px;">Products:</span></strong></legend>
+                <label for="successcount">Qty Produced:</label> <br>
+              <input class="indented" type="text" id="successcount" name="successcount" size="7" value="<?=$successcount ?? ""?>">
             </fieldset><br>
-            <center>
+            <div class="center">
               <input type="hidden" name="id" value="<?=$id?>">
-              <input type="hidden" name="recipe_id" value="<?=$recipe_id?>">
+              <input type="hidden" name="recipe_id" value="<?=$recipe_id ?? ""?>">
               <input type="submit" name="submit" value="Submit Changes">
-            </center>
+            </div>
           </div>
         </div>
       </form>
--- templates/tradeskill/tradeskill.searchresults.tmpl.php
+++ templates/tradeskill/tradeskill.searchresults.tmpl.php
@@ -1,15 +1,15 @@
-      <div class="table_container" style="width:250px;">
-        <div class="table_header">
-          Recipe Search Results
-        </div>
-        <div class="table_content">
-<?if($results != ''):?>
-<?foreach($results as $result): extract($result);?>
-          <a href="index.php?editor=tradeskill&rec=<?=$id?>"><?=$name?></a><br>
-<?endforeach;?>
-<?endif;?>
-<?if($results == ''):?>
-          Your search produced no results!
-<?endif;?>
-        </div>
-      </div>
+<div class="table_container" style="width:250px;">
+    <div class="table_header">
+        Recipe Search Results
+    </div>
+    <div class="table_content">
+        <?php if ($results != ''): ?>
+            <?php foreach ($results as $result): extract($result); ?>
+                <a href="index.php?editor=tradeskill&rec=<?= $id ?>"><?= $name ?></a><br>
+            <?php endforeach; ?>
+        <?php endif; ?>
+        <?php if ($results == ''): ?>
+            Your search produced no results!
+        <?php endif; ?>
+    </div>
+</div>
