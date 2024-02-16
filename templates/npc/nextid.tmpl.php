5886bbbdb5eefde60723ffbe63d3f2371330319d
regneq <rregenn@gmail.com>
2024-02-15 17:59:49 -0800

    Pick first available NPC ID rather than MAX + 1

--- templates/npc/nextid.tmpl.php
+++ templates/npc/nextid.tmpl.php
@@ -1,24 +1,21 @@
-<div class="edit_form" style="width:200px;">
+  <div class="edit_form" style="width:200px;">
     <div class="edit_form_header">
-        Find next available NPCID in
+      Find next available NPCID in
     </div>
     <div class="edit_form_content">
-        <form name="npc_id" method="post"
-              action="index.php?editor=npc&z=<?= $currzone ?? "" ?>&zoneid=<?= $currzoneid ?? "" ?>&npcid=<?= $npcid ?? "" ?>&action=41">
-            <table style="width: 100%;">
-                <tr>
-                    <td style="text-align: left; width: 30%;"><label for="npczoneid">Zone:</label><br/>
-                        <select id="npczoneid" name="npczoneid" style="width:180px;">
-                            <?php foreach ($zoneids as $key => $value): ?>
-                                <option value="<?= $key ?>"<?php echo ($key == $zoneid) ? " selected" : ""; ?>><?= $key ?>
-                                    : <?= $value ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    </td>
-                </tr>
-            </table>
-            <br/><br/>
-            <div class="center"><input type="submit" value="GO"></div>
-        </form>
+      <form name="npc_id" method="post" action="index.php?editor=npc&z=<?=$currzone?>&zoneid=<?=$currzoneid?>&npcid=<?=$npcid?>&action=41">
+        <table width="100%">
+          <tr>
+            <td align="left" width="30%"> Zone:<br>
+              <select name="npczoneid" style="width:180px;">
+<?foreach($zoneids as $key=>$value):?>
+                <option value="<?=$key?>"<?echo ($key == $zoneid)? " selected" : "";?>><?=$key?>: <?=$value?></option>
+<?endforeach;?>
+              </select>
+            </td>
+          </tr>          
+        </table><br><br>
+        <center><input type="submit" value="GO"></center>
+      </form>
     </div>
-</div>
+  </div>
