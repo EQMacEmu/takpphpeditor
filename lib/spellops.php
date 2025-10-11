890e3eeb58181e58d526960c014c81ec3ad114a7
Nazwadi <nazwadi@pm.me>
2025-10-10 22:48:22 -0400

    Adds default variables to templates, removes unused variables

--- lib/spellops.php
+++ lib/spellops.php
@@ -6,7 +6,6 @@ switch ($action) {
     $body->set('currzone', $z);
     $body->set('currzoneid', $zoneid);
     $body->set('npcid', $npcid);
-	$body->set('mysql_class', $mysql_class ?? ""); /* TODO Fix: this variable is not defined anywhere */
     break;
   default:
     break;
